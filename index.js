f920d545b917eccf392da9d3363b70e022ca4394
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-06-13 20:29:58 +0300

    commit tree 32256a6ab4fb5a6e0c0e265fd58519c0d5adc117

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers ){
+   return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+function createFareMultiplier(fr){
+    function unnamed (quad){
+        let quadroupled = fr * quad;
+        return(quadroupled)
+    }
+    return unnamed
+}
+
+function fareDoubler(fare){
+    return fare*2
+}
+
+function fareTripler(fare){
+    return fare*3
+}
+function selectDifferentDrivers(arryOfDrivers, returnDrivers){
+    let selectingDrivers = returnDrivers(arryOfDrivers);return selectingDrivers;
+}
